082d6189a5855ee24e879e860009aa5398a1eb28
Jeff is learning rails and ruby <notskd@jsdf.com>
2019-04-30 23:21:45 +0000

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   root 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  get 'about', to: 'pages#about'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
