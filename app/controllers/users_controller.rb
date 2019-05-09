a1db6b6d6105b3e1bf450db766f29d32f53e50fe
Jeff is learning rails and ruby <notskd@jsdf.com>
2019-05-09 02:20:54 +0000

    add edit update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,30 @@ end
 
 end
 
+def edit
+
+@user = User.find(params[:id])
+
+end
+
+def update
+
+@user = User.find(params[:id])
+
+if @user.update(user_params)
+
+flash[:success] = "Your account was updated successfully"
+
+redirect_to articles_path
+
+else
+
+render 'edit'
+
+end
+
+end
+
 private
 
 def user_params
