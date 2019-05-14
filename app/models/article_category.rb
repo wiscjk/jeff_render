ff09e6a60dd500a11fa01a21a9c932184bfe5b5a
Jeff is learning rails and ruby <notskd@jsdf.com>
2019-05-14 11:22:13 +0000

    add article association

---
+++ app/models/article_category.rb
@@ -0,0 +1,9 @@
+class ArticleCategory < ActiveRecord::Base
+
+# Note Rails 5 -> class ArticleCategory < ApplicationRecord
+
+belongs_to :article
+
+belongs_to :category
+
+end
--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module JeffPoetry
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -22,5 +24,6 @@ module JeffPoetry
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
   end
 end
